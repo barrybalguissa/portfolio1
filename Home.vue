bf09e5d6d479e6cdd0fea9562ea32a920bc9a264
barrybalguissa <122033887+barrybalguissa@users.noreply.github.com>
2024-06-24 16:51:51 +0200

    Add files via upload
    page d'accueil

---
+++ Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <h1 class="title">BIENVENU SUR MON PORTFOLIO</h1>
+  </div>
+</template>
+
+<script>
+import ProjectScript from "./ProjectScript.vue";
+
+export default {
+  name: "HomeSection",
+  ProjectScript,
+};
+</script>
+
+<style scoped>
+.home {
+  font-family: Arial, sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-size: 1.5rem;
+}
+
+.title {
+  color: yellow;
+  position: absolute;
+  right: 400px;
+  top: 0;
+}
+</style>
