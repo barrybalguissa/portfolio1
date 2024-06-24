38867831238f01a037fcd6ee3b2d0bb0038e4a93
barrybalguissa <122033887+barrybalguissa@users.noreply.github.com>
2024-06-24 16:52:54 +0200

    Add files via upload
    page erreur

---
+++ pageErreur.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="not-found">
+    <h1>404</h1>
+    <p>La Page est introuvable</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFoundPage",
+};
+</script>
+
+<style scoped>
+.not-found {
+  background-image: url("@/assets/page-erreur.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  margin: 0;
+  height: 100vh;
+  color: white;
+  text-align: center;
+}
+.not-found img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
