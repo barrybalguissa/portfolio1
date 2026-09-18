7ace57987e16ff54d84400667b7055ba95ab7890
Balguissa Barry <barry>
2026-09-18 13:00:46 +0200

    index.js modifié pour pointer vers components

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../Home.vue';
-import About from '../About.vue';
-import Contact from '../Contact.vue';
-import PageErreur from '../pageErreur.vue';
+import Home from '../components/Home.vue';
+import ProjectScript from '../components/ProjectScript.vue';
+import Contact from '../components/Contact.vue';
+import About from '../components/About.vue';
+import PageErreur from '../components/pageErreur.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
-  { path: '/about', name: 'About', component: About },
+  { path: '/projects', name: 'ProjectScript', component: ProjectScript },
   { path: '/contact', name: 'Contact', component: Contact },
+  { path: '/about', name: 'About', component: About },
   { path: '/:pathMatch(.*)*', name: 'PageErreur', component: PageErreur }
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 });
 
